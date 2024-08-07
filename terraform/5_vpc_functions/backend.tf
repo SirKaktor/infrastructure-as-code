90214a1f4d3ee369f149ab6adc3610fd59083119
SirKaktor <juliuststelldinger@web.de>
2024-08-07 08:57:09 +0200

    cfg

--- terraform/5_vpc_functions/backend.tf
+++ terraform/5_vpc_functions/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "23-10-bucket-beispiel" # Hier eigenen Bucket eintragen
+    bucket = "<bucket-name>" # Hier eigenen Bucket eintragen
     key    = "vpc/vpc.tfstate"
     region = "eu-central-1"
   }
